984b29825493c87d39942468553ba25fd22ed674
Faik Eyvazli <faikeyvazovvv@gmail.com>
2025-12-08 17:57:17 +0400

    Final version

--- main.py
+++ main.py
@@ -23,8 +23,18 @@ class Container:
         c._redistribute()
 
     def addWater(self, amt):
-        #add to this container then redistribute
-        self.amount += amt
+        try:
+            if (amt < 0):
+                raise ValueError("Negative amount cannnot be added")
+                
+            #add to this container then redistribute
+            else:
+                self.amount += amt
+        except ValueError as ve:
+            print(ve)
+            exit()
+            
+            
         self._redistribute()
 
     def _redistribute(self):
@@ -45,59 +55,68 @@ class Container:
         for x in visited:
             x.amount = share
             
-                   
-print("Creating 5 containers: A, B, C, D, E")
-A = Container()
-B = Container()
-C = Container()
-D = Container()
-E = Container()
+def main():                   
+    print("Creating 5 containers: A, B, C, D, E")
+    A = Container()
+    B = Container()
+    C = Container()
+    D = Container()
+    E = Container()
 
-print(f"Adding 10 to A")
-A.addWater(10)
-print(f"Adding 15 to B")
-B.addWater(15)
-print(f"Adding 20 to C")
-C.addWater(20)
-print(f"Adding 2 to D")
-D.addWater(2)
-print(f"Adding 12 to E")
-E.addWater(12)
+    print(f"Adding 10 to A")
+    A.addWater(10)
+    print(f"Adding 15 to B")
 
-print('-------')
+    B.addWater(-15)
 
+    print("Error is thrown")
+        
+        
+    print(f"Adding 20 to C")
+    C.addWater(20)
+    print(f"Adding 2 to D")
+    D.addWater(2)
+    print(f"Adding 12 to E")
+    E.addWater(12)
 
-print(f"Water in A: {A.amount}")
-print(f"Water in B: {B.amount}")
-print(f"Water in C: {C.amount}")
-print(f"Water in D: {D.amount}")
-print(f"Water in E: {E.amount}")
-
-print('-------')
-
-print("Connecting A to B and A to C")
-A.connectTo(B)
-A.connectTo(C)
-print('-------')
-
-print(f'Water in A: {A.amount}')
-print(f"Water in B: {B.amount}")
-print(f"Water in C: {C.amount}")
-
-print('-------')
-
-print("Connecting D to E")
-print('-------')
-
-D.connectTo(E)
-print(f"Water in D: {D.amount}")
-print(f"Water in E: {E.amount}")
-
-print('-------')
-print("Connecting ABC to DE")
-print('-------')
+    print('-------')
 
 
-A.connectTo(D)
-print(f'Water in A: {A.amount}')
-print(f'Water in D: {D.amount}')
+    print(f"Water in A: {A.amount}")
+    print(f"Water in B: {B.amount}")
+    print(f"Water in C: {C.amount}")
+    print(f"Water in D: {D.amount}")
+    print(f"Water in E: {E.amount}")
+
+    print('-------')
+
+    print("Connecting A to B and A to C")
+    A.connectTo(B)
+    A.connectTo(C)
+    print('-------')
+
+    print(f'Water in A: {A.amount}')
+    print(f"Water in B: {B.amount}")
+    print(f"Water in C: {C.amount}")
+
+    print('-------')
+
+    print("Connecting D to E")
+    print('-------')
+
+    D.connectTo(E)
+    print(f"Water in D: {D.amount}")
+    print(f"Water in E: {E.amount}")
+
+    print('-------')
+    print("Connecting ABC to DE")
+    print('-------')
+
+
+    A.connectTo(D)
+    print(f'Water in A: {A.amount}')
+    print(f'Water in D: {D.amount}')
+
+
+if __name__ == '__main__':
+    main()
